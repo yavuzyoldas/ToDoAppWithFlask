49179124a078798f30e3efa3fb3c068f5593d7c2
Yavuz Yoldas <yavuzahmetyoldas@gmail.com>
2021-03-01 11:21:51 +0300

    A minor error is fixed.

--- app/models.py
+++ app/models.py
@@ -19,26 +19,4 @@ class Todo(Base): # Account Kit, Accelerate Kit etc
         self.todos_title = todos_title
         self.todos_content = todos_content
         self.todos_complete = todos_complete
-
-class User(Base): # it can be android, ios, flutter etc.
-    # See http://flask-sqlalchemy.pocoo.org/2.0/models/#simple-example
-    # for details on the column types.
-    __tablename__ = 'users'
-    # We always need an id
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    todo_id = Column(Integer, ForeignKey("todos.id"))
-    kit = relationship("Todo", back_populates='users')
-    created_date = Column(DateTime, default=datetime.utcnow)
-    def __init__(self, name):
-        self.name = name
-        self.created_date = datetime.now() 
-    
-
-
-""" class Todo(db.Model):
-   __tablename__ = "todos"
-   id = db.Column(db.Integer, primary_key=True)
-   title = db.Column(db.String(80))
-   content = db.Column(db.Text)
-   complete = db.Column(db.Boolean)"""       
+     
